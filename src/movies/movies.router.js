5f7c8e8555d2c16de0077db880d7606c98392877
ErynGalatea <aarontsai21@gmail.com>
2022-10-04 12:28:57 -0400

    update2

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/movies").get(controller.list).all(methodNotAllowed);
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 router
   .route("/:movieId/theaters")
